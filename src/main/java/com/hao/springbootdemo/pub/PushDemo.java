cf7803ccb267a24c0a01025900890d0d668f0b5f
xu.liang <1262650422@qq.com>
2023-01-10 09:09:54 +0800

    fix：测试

---
+++ src/main/java/com/hao/springbootdemo/pub/PushDemo.java
@@ -0,0 +1,15 @@
+package com.hao.springbootdemo.pub;
+
+/**
+ * @author xu.liang
+ * @since 2023/1/10 09:08
+ */
+public class PushDemo {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
