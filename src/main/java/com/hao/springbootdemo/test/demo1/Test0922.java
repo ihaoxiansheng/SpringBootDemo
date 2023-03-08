f3e08c94ad1d53d224b94913405adf0faca81cf2
xu.liang <1262650422@qq.com>
2023-03-08 09:01:55 +0800

    fix：提交GSON

--- src/main/java/com/hao/springbootdemo/test/demo1/Test0922.java
+++ src/main/java/com/hao/springbootdemo/test/demo1/Test0922.java
@@ -1,9 +1,14 @@
 package com.hao.springbootdemo.test.demo1;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.hao.springbootdemo.test.demo3.DataTypeAdaptor;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 /**
@@ -11,6 +16,12 @@ import java.util.function.Predicate;
  * @since 2022/9/22 10:26
  */
 public class Test0922 {
+
+    private final static Gson GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss.SSS").create();
+    private final static Gson GSON1 =
+            new GsonBuilder().registerTypeAdapter(new TypeToken<Map<String,Object>>(){}.getType(),new DataTypeAdaptor()).setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
+
     public static void main(String[] args) {
 
         Predicate<String> predicate = list -> list.contains("好");
@@ -42,6 +53,20 @@ public class Test0922 {
         boolean b = strings2.containsAll(strings);
         System.out.println("b = " + b);
 
+        GSON.toJson(new Date(), System.out);
+        System.out.println();
+        String dateStr = GSON.toJson(new Date());
+        System.out.println("dateStr = " + dateStr);
+
+        Date date = new Date();
+        String dateJson = GSON.toJson(date);
+        System.out.println(date);
+        System.out.println("date  = " + date.getTime());
+        Date date2 = GSON.fromJson(dateJson, Date.class);
+        System.out.println(date2);
+        System.out.println("date2 = " + date2.getTime());
+        System.out.println(date2.equals(date));// 定义时间是yyyy-MM-dd HH:mm:ss.SSS相等，定义为yyyy-MM-dd HH:mm:ss不相等
+        System.out.println(date2 == date);
 
     }
 }
