00521e6a1dc33e4b9849e79045c5cbc7be9388be
xu.liang <1262650422@qq.com>
2023-03-23 09:21:13 +0800

    fix：提交UserDTO

---
+++ src/main/java/com/hao/springbootdemo/dto/UserDTO.java
@@ -0,0 +1,36 @@
+package com.hao.springbootdemo.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author xu.liang
+ * @since 2023/3/11 23:00
+ */
+@Data
+public class UserDTO {
+
+    @NotNull(message = "id不能为空")
+    private String id;
+
+    @NotBlank(message = "用户名不能为空")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+    @NotBlank(message = "邮箱不能为空")
+    private String email;
+
+    @NotBlank(message = "手机号不能为空")
+    private String phone;
+
+    @NotBlank(message = "地址不能为空")
+    private String address;
+
+    @NotBlank(message = "描述不能为空")
+    private String desc;
+
+}
