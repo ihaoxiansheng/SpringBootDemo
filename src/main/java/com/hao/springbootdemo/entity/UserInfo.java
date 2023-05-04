eb317a769e0e98c953ac96c95117c8c59fb45aab
xu.liang <ihaoxiansheng@163.com>
2023-05-04 15:06:23 +0800

    fix：提交CollectionToMap

---
+++ src/main/java/com/hao/springbootdemo/entity/UserInfo.java
@@ -0,0 +1,16 @@
+package com.hao.springbootdemo.entity;
+
+import lombok.Data;
+
+/**
+ * @author xu.liang
+ * @since 2023/4/22 14:43
+ */
+@Data
+public class UserInfo {
+
+    private String userId;
+
+    private String age;
+
+}
