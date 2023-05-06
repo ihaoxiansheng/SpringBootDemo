e3b89fd5c34aff491d912c82f8d8af78144dae4a
xu.liang <ihaoxiansheng@163.com>
2023-05-06 18:00:30 +0800

    fix：提交@Builder

---
+++ src/main/java/com/hao/springbootdemo/entity/User1.java
@@ -0,0 +1,24 @@
+package com.hao.springbootdemo.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author xu.liang
+ * @since 2023/05/04 15:03
+ */
+@Data
+@Builder(toBuilder = true)
+public class User1 {
+
+    private String id;
+
+    private String name;
+
+    private String age;
+
+    private String email;
+
+    private UserInfo userInfo;
+
+}
