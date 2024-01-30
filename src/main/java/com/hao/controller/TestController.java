b8f425fe9f02a19db82fae80796624e1cd664eb1
haoxiansheng <ihaoxiansheng@163.com>
2024-01-30 14:35:13 +0800

    fix：提交测试创建单号

--- src/main/java/com/hao/controller/TestController.java
+++ src/main/java/com/hao/controller/TestController.java
@@ -5,13 +5,17 @@ import com.hao.util.exception.GlobalExceptionHandler;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -36,6 +40,9 @@ public class TestController {
 //    private Integer connectTimeout;
 
     @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource
     private JdbcTemplate jdbcTemplate;
 
     @GetMapping("/hello")
@@ -58,6 +65,26 @@ public class TestController {
         List<String> list = new ArrayList<>();
         // list.get(0);
         return result;
+    }
+
+    @GetMapping("/createNoTest")
+    public void createNoTest() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+        String nowTime = dateFormat.format(new Date());
+        StringBuilder key = new StringBuilder("NO_");
+        key.append(nowTime);
+        Long increment = redisTemplate.opsForValue().increment(key.toString());
+        if (Long.valueOf(1).equals(increment)) {
+            // 如果获取主键为1则设置主键有效时长为一天86400秒
+            redisTemplate.expire(key.toString(), Duration.ofDays(1));
+
+        }
+        // 四位数，不够补零
+        String primaryNum = String.format("%05d", increment);
+        StringBuilder sheetNo = new StringBuilder(nowTime);
+        sheetNo.append(primaryNum);
+        String no = "M" + sheetNo;
+        System.out.println("no = " + no);
     }
 
     public static void main(String[] args) {
