2c3cfacb8576307a957ea3dc2208dfd0b4a6456c
haoxiansheng <ihaoxiansheng@163.com>
2024-08-18 15:52:03 +0800

    fix：swagger优化

--- src/main/java/com/hao/config/SwaggerBeanConfig.java
+++ src/main/java/com/hao/config/SwaggerBeanConfig.java
@@ -2,6 +2,7 @@ package com.hao.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,14 +17,14 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @since 2023/04/13 15:43
  */
 @Configuration
-public class SwaggerConfig {
+public class SwaggerBeanConfig implements WebMvcConfigurer {
 
     // Docket bean definition （方法名不可重复）
     @Bean
     public Docket haoApi() {
         // configuring the API documentation settings
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("好")
+                .groupName(Docket.DEFAULT_GROUP_NAME)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.hao"))
 //                .paths(PathSelectors.ant("/api/*"))
@@ -35,7 +36,7 @@ public class SwaggerConfig {
     // setting the API information
     private ApiInfo apiInfo() {
 
-        return new ApiInfoBuilder().title("Demo API") // 网页标签镖旗
+        return new ApiInfoBuilder().title("Demo API") // 网页标签标题
                 .description("API documentation for Demo project")
                 .version("1.0.0")
                 .license("Apache 2.0")
