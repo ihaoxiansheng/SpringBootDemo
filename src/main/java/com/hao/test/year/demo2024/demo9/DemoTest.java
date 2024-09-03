3bbb263f0a6c73ab89b4a772ae26b69e17cb566b
haoxiansheng <ihaoxiansheng@163.com>
2024-09-03 14:33:56 +0800

    feat：DemoTest提交

---
+++ src/main/java/com/hao/test/year/demo2024/demo9/DemoTest.java
@@ -0,0 +1,16 @@
+package com.hao.test.year.demo2024.demo9;
+
+/**
+ * @author xu.liang
+ * @since 2024/9/2 09:54
+ */
+public class DemoTest {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
